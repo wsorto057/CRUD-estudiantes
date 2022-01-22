d2f515260926c16464aaec10484545d2ba3a2965
William_Sorto <william.sorto.alv@gmail.com>
2022-01-22 00:19:43 -0600

    commit para segunda rama sabado

---
+++ com.app.crud/src/main/java/com/app/web/servicio/EstudianteServicioImpl.java
@@ -0,0 +1,45 @@
+package com.app.web.servicio;
+
+import java.util.List;
+import com.app.web.entidad.Estudiante;
+import com.app.web.repositorio.EstudianteRepositorio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstudianteServicioImpl implements EstudianteServicio {
+
+	
+	@Autowired
+	private EstudianteRepositorio repositorio;
+	
+	@Override
+	public List<Estudiante> listarEstudiantes() {
+		
+		return repositorio.findAll();
+	}
+
+	@Override
+	public Estudiante guardarEstudiante(Estudiante estudiante) {
+		
+		return repositorio.save(estudiante);
+	}
+
+	@Override
+	public Estudiante obtenerEstudiantePorId(Long id) {
+		return repositorio.findById(id).get();
+	}
+
+	@Override
+	public Estudiante actualizarEstudiante(Estudiante estudiante) {
+		return repositorio.save(estudiante);
+	}
+
+	@Override
+	public void eliminarEstudiante(Long id) {
+		repositorio.deleteById(id);
+		
+	}
+
+}

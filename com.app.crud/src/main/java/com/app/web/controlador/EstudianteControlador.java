0b9f0f9dfe85e10645a72411bd817da5947755cf
William_Sorto <william.sorto.alv@gmail.com>
2022-01-26 20:38:01 -0600

    commit para repo insorto_ide

---
+++ com.app.crud/src/main/java/com/app/web/controlador/EstudianteControlador.java
@@ -0,0 +1,144 @@
+package com.app.web.controlador;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.app.web.entidad.Estudiante;
+import com.app.web.servicio.EstudianteServicio;
+
+
+
+@Controller
+public class EstudianteControlador {
+	
+	@Autowired
+	private EstudianteServicio servicio;
+	
+	
+	@GetMapping({ "/estudiantes","/" })
+	public String listarEstudiantes(Model modelo) {
+		modelo.addAttribute("estudiantes", servicio.listarEstudiantes());
+		return "estudiantes";
+	}
+	
+	
+	@GetMapping("/estudiantes/nuevo")
+	public String crearEstudiante(Model modelo) {
+		Estudiante estudiante= new Estudiante();
+		modelo.addAttribute("estudiante", estudiante);
+		return "crear_estudiante";
+	}
+	
+	@PostMapping("/estudiantes")
+	public String guardarEstudiante(@ModelAttribute("estudiante") Estudiante estudiante) {
+		servicio.guardarEstudiante(estudiante);
+		return "redirect:/estudiantes ";
+	}
+	
+	/*
+	@GetMapping("/estudiantes/editatr/{id}")
+	public String mostrarEditar(@PathVariable Long id, Model modelo) {
+		modelo.addAttribute("estudiante", servicio.obtenerEstudiantePorId(id));
+		return "editar_estudiante";
+	}
+	
+	@PostMapping("/estudiantes/{id}")
+	public String actualizarEstudiante(@PathVariable Long id, @ModelAttribute("estudiante") Estudiante estudiante, Model modelo) {
+		Estudiante estudianteExistente= servicio.obtenerEstudiantePorId(id);
+		estudianteExistente.setId(id);
+		estudianteExistente.setNombre(estudiante.getNombre());
+		estudianteExistente.setApellido(estudiante.getApellido());
+		estudianteExistente.setEmail(estudiante.getEmail());
+		
+		servicio.actualizarEstudiante(estudianteExistente);
+		
+		return "redirect:/estudiantes";
+	}
+	*/
+	
+	@GetMapping("/estudiantes/editar/{id}")
+	public String mostrarEditar(@PathVariable Long id, Model modelo) {
+		modelo.addAttribute("estudiante", servicio.obtenerEstudiantePorId(id));
+		return "editar_estudiante";
+	}
+	
+	@PostMapping("/estudiantes/{id}")
+	public String actualizarEstudiante(@PathVariable Long id, @ModelAttribute("estudiante") Estudiante estudiante, Model modelo) {
+		Estudiante estudianteExistente = servicio.obtenerEstudiantePorId(id);
+		estudianteExistente.setId(id);
+		estudianteExistente.setNombre(estudiante.getNombre());
+		estudianteExistente.setApellido(estudiante.getApellido());
+		estudianteExistente.setEmail(estudiante.getEmail());
+
+		servicio.actualizarEstudiante(estudianteExistente);
+		return "redirect:/estudiantes";
+	}
+
+	@GetMapping("/estudiantes/{id}")
+	public String eliminarEstudiante(@PathVariable Long id) {
+		servicio.eliminarEstudiante(id);
+		return "redirect:/estudiantes";
+	}
+	
+	// prueba cambio de ramas 
+	public void hola_mundo () {
+		System.out.println("hola mundo");
+	}
+	
+	public void hola_mundo_2 () {
+		System.out.println("hola mundo");
+	}
+	
+	
+	// segunda prueba cambio de ramas 
+	public void hola_mundo_3 () {
+		System.out.println("hola mundo");
+	}
+	
+	public void hola_mundo_4 () {
+		System.out.println("hola mundo");
+	}
+	
+	// segunda prueba cambio de ramas dia domingo
+	public void hola_mundo_5 () {
+		System.out.println("hola mundo");
+	}
+	
+	public void hola_mundo_6 () {
+		System.out.println("hola mundo");
+	}
+	
+	
+	// segunda prueba cambio de ramas dia domingo para dos
+	public void hola_mundo_7 () {
+		System.out.println("hola mundo");
+	}
+	
+	public void hola_mundo_8 () {
+		System.out.println("hola mundo");
+	}
+	
+	
+	// segunda prueba cambio de ramas miercols con comandos git
+	public void hola_mundo_9 () {
+		System.out.println("hola mundo");
+	}
+	
+	public void hola_mundo_10 () {
+		System.out.println("hola mundo");
+	}
+	
+	// segunda prueba cambio de ramas miercoles con java IDE
+		public void hola_mundo_11 () {
+			System.out.println("hola mundo");
+		}
+		
+		public void hola_mundo_12 () {
+			System.out.println("hola mundo");
+		}
+}
---
+++ com.app.crud/src/main/java/com/app/web/entidad/nueva.java
@@ -0,0 +1,29 @@
+package com.app.web.entidad;
+
+public class nueva {
+	private int id;
+	private String nombre;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public nueva(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+	public nueva() {
+		super();
+	}
+	
+	
+
+}
